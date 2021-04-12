21aa563379f0fe33f288088f8021f41aad7ac9fb
JacobRPrice <JacobRPricePhD@gmail.com>
2021-04-12 13:12:09 -0400

    add Bioinformatics_Pipeline.sh

---
+++ Bioinformatics_Pipeline.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+###
+# copy raw data and other contextual information to project data directory 
+###
+
+
+###
+# 16S dada2 pipeline
+###
+
+date
+Rscript ./R/bioinformatics/16S_QC.R
+
+date
+Rscript ./R/bioinformatics/16S_dada_F.R
+
+# date
+#Rscript ./R/bioinformatics/16S_dada_R.R 
+
+# date
+# Rscript ./R/bioinformatics/16S_dada_join.R
+
+# date
+# Rscript ./R/bioinformatics/16S_Taxonomy.R
+
+# date
+#Rscript ./R/bioinformatics/16S_PhyTree.R
+
+# date
+#Rscript ./R/bioinformatics/16S_SampleData.R
+
+# date
+#Rscript ./R/bioinformatics/16S_Phyloseq.R
+
+
+
+###
+# ITS dada2 pipeline
+###
+
+# date
+#Rscript ./R/bioinformatics/ITS_QC.R
+
+# date
+# Rscript ./R/bioinformatics/ITS_dada_F.R 
+
+# date
+# Rscript ./R/bioinformatics/ITS_dada_R.R 
+
+# date
+# Rscript ./R/bioinformatics/ITS_dada_join.R
+
+# date
+# Rscript ./R/bioinformatics/ITS_Taxonomy.R
+
+# date
+#Rscript ./R/bioinformatics/ITS_PhyTree.R
+
+# date
+# Rscript ./R/bioinformatics/ITS_SampleData.R
+
+# date
+#Rscript ./R/bioinformatics/ITS_Phyloseq.R
